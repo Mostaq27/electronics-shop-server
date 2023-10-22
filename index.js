f6bc68efb23dd57bdc2fa01b327b14cc51a1bdd1
Mostaq27 <mostaq99muzahid@gmail.com>
2023-10-22 16:10:28 +0600

    get cart data from mongoDB

--- index.js
+++ index.js
@@ -84,6 +84,12 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/cart', async(req, res) =>{
+            const cursor = cartCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
 
 
         // Send a ping to confirm a successful connection
